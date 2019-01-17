9fecad2cf7b72572074a3c764fb11bf091dc3356
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-01-17 16:23:30 +0100

    committing PrintElements

---
+++ week-01/day-03/PrintElements/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int numbers[4]={4, 5, 6, 7};
+
+
+    std::cout << numbers[0] << ", " << numbers[1] << ", " << numbers[2] << ", " << numbers[3] << std::endl;
+
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `numbers`
+
+    return 0;
+}
