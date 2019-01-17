ca5288069d8952f279b9b907c3fc4a065f62dc79
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-01-17 11:38:25 +0100

    adding Doubling

---
+++ week-01/day-03/Doubling/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+void doubling(int x){
+    x*=2;
+    std::cout << x << std::endl;
+}
+
+int main(int argc, char* args[]) {
+
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    // - Print the result of `doubling(baseNum)`
+    int baseNum = 123;
+    doubling(baseNum);
+
+    return 0;
+}
