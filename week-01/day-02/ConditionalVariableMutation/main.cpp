5f2ed06f3da4d25bca60dcc0bd6ec450eae105b4
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-01-18 14:40:07 +0100

    committing ConditionalVariableMutation

--- week-01/day-02/ConditionalVariableMutation/main.cpp
+++ week-01/day-02/ConditionalVariableMutation/main.cpp
@@ -3,11 +3,10 @@
 
 int main(int argc, char* args[]) {
 
-    double a = 24;
+    int a = 24;
     int out = 0;
     // if a is even increment out by one
 
-    bool
     if (a % 2 == 0) {
         out += 1;
 
@@ -33,7 +32,12 @@ int main(int argc, char* args[]) {
 
     int c = 123;
     int credits = 100;
-    bool isBonus = false;
+    bool isBonus = true;
+    if ((credits >= 50) and !isBonus) {
+        c -=2;
+    }else if ((credits < 50) and !isBonus){
+        c -=1;
+    }
     // if credits are at least 50,
     // and isBonus is false decrement c by 2
     // if credits are smaller than 50,
@@ -46,6 +50,13 @@ int main(int argc, char* args[]) {
     int d = 8;
     int time = 120;
     std::string out3 = "";
+    if (time > 200){
+        out3 = "Time out";
+    }else if ((d % 4 == 0) and (time <= 200)){
+        out3 = "check";
+    }else{
+        out3 = "Run Forest Run!";
+    }
     // if d is dividable by 4
     // and time is not more than 200
     // set out3 to "check"
