ee7453814b27724cc79befe55f8ed354d75db113
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-01-22 18:46:12 +0100

    lookForMaximum

---
+++ week-02/day-02/lookForMaximum/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+
+    // Create a program which first asks for a number
+    // this number indicates how many integers we want to store in an array
+    // and than asks for numbers till the user fills the array
+    // It should print out the biggest number in the given array and the memory address of it
+
+
+    int givenNumber;
+    int array[givenNumber];
+    int big;
+
+
+    std::cout << "Please give me a number: " << std::endl;
+    std::cin >> givenNumber;
+
+    std::cout << "Now give me " << givenNumber << " number(s): " << std::endl;
+
+    for (int i = 0; i < givenNumber; i++){
+
+
+
+        std::cin >> array[i];
+
+        big = array[i];
+
+
+        if (big < array[i]) {
+
+            big = array[i];
+        }
+
+    }
+
+    std::cout << "The biggest given number is: " << big << std::endl;
+
+    std::cout << "This number is on the memory address: " << &array[big];
+
+
+    return 0;
+}
