8ec22cb22f2d110ad673fe205f87d988d132e125
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-01-30 16:37:43 +0100

    committing dominoes

---
+++ week-03/day-03/dominoes/Domino.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Snöcc on 2019-01-30.
+//
+#include "Domino.h"
+
+Domino::Domino(int valueA, int valueB)
+{
+    _values = std::make_pair(valueA, valueB);
+}
+
+std::pair<int,int> Domino::getValues()
+{
+    return _values;
+}
+
+std::string Domino::toString()
+{
+    return "[" + std::to_string(_values.first) + ", " + std::to_string(_values.second) + "]";
+}
+
