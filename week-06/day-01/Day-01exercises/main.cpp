90b2e453345a03d12f8f7127148345a6f3fdb5df
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-02-23 14:49:23 +0100

    exercise 5 is done

--- week-06/day-01/Day-01exercises/main.cpp
+++ week-06/day-01/Day-01exercises/main.cpp
@@ -3,10 +3,11 @@
 
 int main()
 {
+    // HELLO ME
     // Modify this program to greet you instead of the World!
     printf("Hello, Me!");
 
-
+    // HELLO OTHERS
     // Greet 3 of your classmates with this program, in three separate lines
     // like:
     //
@@ -22,6 +23,7 @@ int main()
     printf("Hello, %s!\n", Gyuri);
     printf("Hello, %s!\n\n", Adam);
 
+    // INTRODUCE YOURSELF
     // Write a program that prints a few details to the terminal window about you
     // It should print each detail to a new line.
     //  - Your name
@@ -43,6 +45,7 @@ int main()
     printf("%d\n", age);
     printf("%.2f\n\n", heigth);
 
+    //TWO NUMBERS
     // Create a program that prints a few operations on two numbers: 22 and 13
 
     // Print the result of 13 added to 22
@@ -71,5 +74,28 @@ int main()
     printf("%d\n", d);
     printf("%d\n", e);
 
+    // CUBOID
+    // Write a program that stores 3 sides of a cuboid as variables (doubles)
+    // You should get these variables via console input
+    // The program should write the surface area and volume of the cuboid like:
+    //
+    // Surface Area: 600
+    // Volume: 1000
+
+    double cuboidA;
+    double cuboidB;
+    double cuboidC;
+
+
+    printf("Please give me 1 side of the cuboid: \n");
+    scanf("%lf", &cuboidA);
+    printf("Please give me another side of the cuboid: \n");
+    scanf("%lf", &cuboidB);
+    printf("Please give me the 3rd side of the cuboid: \n");
+    scanf("%lf", &cuboidC);
+    double surface = 2 * (cuboidA * cuboidB + cuboidA * cuboidC + cuboidB * cuboidC);
+    double volume = cuboidA * cuboidB * cuboidC;
+    printf("Surface Area: %.2lf\n", surface);
+    printf("Volume: %.2lf\n", volume);
     return 0;
 }
