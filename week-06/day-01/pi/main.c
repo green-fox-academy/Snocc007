d1ee8900d06ed173fb0d360400d781bf65353ee8
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-02-25 12:47:12 +0100

    separate is done

--- week-06/day-01/pi/main.c
+++ week-06/day-01/pi/main.c
@@ -2,12 +2,7 @@
 
 #define PI 3.14
 
-double areaOfCircle(double radius)
-{
-    double area;
-    area = radius * radius * PI;
-    return area;
-}
+#include "pi.h"
 
 int main()
 {
@@ -17,7 +12,31 @@ int main()
     // and return the area of that cirle
     // area = radius * radius * PI
 
-    printf("%.3lf", areaOfCircle(5.5));
 
+    // Continue working on the pi.c project
+
+    // Organise the function from the previous excercise to separate .c and .h files
+    // Create another function which calculates the circumference of a circle
+    // the radius of the circle should be passed as a parameter
+    // the function should return the calculated circumference
+    // circumference = 2 * radius * PI
+    // this function should be in the same .c and .h files as the one which calculates the area
+
+    printf("The area of the circle is: %.3lf\n", areaOfCircle(5.5));
+    printf("The circumreference of the circle is: %.3lf\n", circumreference(5.5));
     return 0;
+}
+
+double areaOfCircle(double radius)
+{
+    double area;
+    area = radius * radius * PI;
+    return area;
+}
+
+double circumreference(double radius)
+{
+    double circumreference;
+    circumreference = 2 * radius * PI;
+    return  circumreference;
 }
---
+++ week-06/day-01/pi/pi.h
@@ -0,0 +1,11 @@
+//
+// Created by Snöcc on 2019-02-25.
+//
+
+#ifndef PI_PI_H
+#define PI_PI_H
+
+double areaOfCircle(double radius);
+double circumreference(double radius);
+
+#endif //PI_PI_H
