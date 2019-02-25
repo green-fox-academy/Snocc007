c5d12adbeb9fb247e68315561eb941b3531240dc
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-02-25 11:43:06 +0100

    helloOthers is done

---
+++ week-06/day-01/helloOthers/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // HELLO OTHERS
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Gyuri!
+    // Hello, Boro!
+    // Hello, Adam!
+
+    char Boro[] = "Boro";
+    char Gyuri[] = "Gyuri";
+    char Adam[] = "Adam";
+
+    printf("Hello, %s!\n", Boro);
+    printf("Hello, %s!\n", Gyuri);
+    printf("Hello, %s!\n\n", Adam);
+
+    return 0;
+}
