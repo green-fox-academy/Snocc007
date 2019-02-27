95c0b0b32639dca9bdc52808f00fd2d2ed1c4d15
Istvan Schneider <kellmegegyfiok@hotmail.com>
2019-02-27 13:13:05 +0100

    deallocate is done

---
+++ week-06/day-03/deallocate/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with malloc
+// please allocate a 10 long array and fill it with even numbers then print the whole array
+// please deallocate memory without using free
+
+
+int main()
+{
+    int* pointer = NULL;
+    pointer = (int *) malloc(10 * sizeof(int));
+    for (int i = 0; i < 5; i++) {
+        pointer[i] = i;
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", pointer[i]);
+    }
+    realloc(pointer, 0);
+
+
+
+    return 0;
+}
